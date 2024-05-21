a4e01e5f96ee5468ea35693cf93fdf6d1a8a4aa6
lillian <keynesiane@gmail.com>
2024-05-21 16:57:18 -0700

    added blog homepage displaying posts

--- src/home.jsx
+++ src/home.jsx
@@ -3,15 +3,19 @@ import { Link } from "react-router-dom";
 import { Header } from "./header";
 
 const truncateString = (string) => {
-
+    return string.slice(0, 60) + "..."
 }
 
 const BlogCard = ({title, author, id, date_posted, content}) => {
-    return (<div>
-        <h2>{title}</h2>
+    return (
+    <Link to={`${id}`}><div className="flex flex-col gap-y-2.5">
+        <h2 className="text-center">{title}</h2>
+        <div className="flex gap-x-4">
         <p>{date_posted}</p>
         <p>{author}</p>
-    </div>)
+        </div>
+        <p>{truncateString(content)}</p>
+    </div></Link>)
 }
 
 export const HomePage = () => {
@@ -23,8 +27,8 @@ export const HomePage = () => {
         const getPost = async () => {
         try {
             const response = await fetch("https://blog-api-backend.fly.dev/blog/", {mode: "cors"});
-            const data = response.json();
-            setPosts(data)
+            const data = await response.json();
+            setPosts(data);
         }
         catch(error) {
             setError(error);
@@ -37,10 +41,23 @@ export const HomePage = () => {
         getPost();
     }, [])
 
-    if (error) return <p></p>;
-    if (loading) return <div></div>
+    const cards = !error && !loading ? posts.map((post) => {
+        return <div key={post._id}>
+            <BlogCard
+            title={post.title}
+            author={post.author.username}
+            id={post._id}
+            date_posted={post.date_posted_formatted}
+            content={post.content}/>
+            </div>
+    }) : null;
+
+    if (error) return <p>An error was encountered</p>;
+    if (loading) return <div>Loading...</div>
 
     return (<div>
         <Header></Header>
+        <div className="pt-5"><h1 className="text-center">All Posts</h1></div>
+        <div className="flex flex-col items-center gap-y-6 pt-5">{cards}</div>
     </div>)
 }
